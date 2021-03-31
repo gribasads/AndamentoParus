11a7dd990176ef485f19eb6d32e2adb7a9c00d09
gribasads <gustavoribas.ads@gmail.com>
2021-03-31 14:38:19 -0300

    Commit 13 criado Zuul Gateway

---
+++ ws-sts/hr-api-gateway-zuul/src/main/java/com/devsuperior/hrapigatewayzull/com/devsuperior/hrapigatewayzull/HrApiGatewayZullApplication.java
@@ -0,0 +1,16 @@
+package com.devsuperior.hrapigatewayzull.com.devsuperior.hrapigatewayzull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableEurekaClient
+@EnableZuulProxy
+@SpringBootApplication
+public class HrApiGatewayZullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrApiGatewayZullApplication.class, args);
+	}
+}

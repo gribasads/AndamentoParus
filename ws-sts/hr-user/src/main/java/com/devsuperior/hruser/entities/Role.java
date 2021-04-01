9799769c340dca06de466b99d7ce5246b47fc2c6
gribasads <gustavoribas.ads@gmail.com>
2021-04-01 10:08:14 -0300

    Commit23 aplicando many to many em hr-user

---
+++ ws-sts/hr-user/src/main/java/com/devsuperior/hruser/entities/Role.java
@@ -0,0 +1,89 @@
+package com.devsuperior.hruser.entities;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="tb_role")
+public class Role implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String rolename;
+	
+	@ManyToMany(fetch=FetchType.EAGER)
+	@JoinTable(name="tb_user-role",joinColumns=@JoinColumn(name="user_id"),
+	inverseJoinColumns = @JoinColumn(name= "role_id"))
+	private Set<Role>roles = new HashSet<>();
+	public Role() {}
+	
+	public Role(long id, String rolename) {
+		super();
+		this.id = id;
+		this.rolename = rolename;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getRolename() {
+		return rolename;
+	}
+	public void setRolename(String rolename) {
+		this.rolename = rolename;
+	}
+	
+	
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((rolename == null) ? 0 : rolename.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		if (rolename == null) {
+			if (other.rolename != null)
+				return false;
+		} else if (!rolename.equals(other.rolename))
+			return false;
+		return true;
+	}
+	
+	
+
+}
